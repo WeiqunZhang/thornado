099ff89112016d438464d1246931c30a4afee343
Weiqun Zhang <weiqunzhang@lbl.gov>
2023-04-22 09:18:33 -0700

    Debug CI
    * add --debug
    
    * remove comments

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 make clean
-make > make.ou
+make DEP_CHECK_OPTS=--debug > make.ou
+ls .
 cat make.ou
+cat dependencies.out
 cat tmp_build_dir/d/1d.gnu.DEBUG.MPI.exe/f90.depends
 make help
 make print-F90EXE_sources
